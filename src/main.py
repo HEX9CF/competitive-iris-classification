d1e9639e02aee443976b816709a6d2573638ce45
HEX9CF <hex9cf@aliyun.com>
2024-12-04 21:54:13 +0800

    Load data

---
+++ src/main.py
@@ -0,0 +1,18 @@
+import pandas as pd
+
+# 读取数据
+headers = ['sepal_length', 'sepal_width', 'petal_length', 'petal_width', 'class']
+data = pd.read_csv(filepath_or_buffer='../data/data.csv', header=None, names=headers)
+
+print(data)
+
+# 归一化
+
+# 训练竞争神经网络
+
+# 欧几里得距离判定收敛
+
+
+
+
+
